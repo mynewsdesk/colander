67cb302448f67122cc490ebb651ecc91997509ad
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-11-25 13:45:44 +0100

    Add support for top-level-domains > 4 chars
    I.e `.travel` and `.museum`
    http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains

--- spec/plain_spec.rb
+++ spec/plain_spec.rb
@@ -5,7 +5,13 @@ describe Colander::Parser::Plain do
     it "retreives emails from an csv file" do
       parser = Colander::Parser::Plain.new("spec/fixtures/plain.csv")
       parser.parse
-      parser.emails.sort.should eql(["jonas.forsberg@mynewsdesk.com", "peter.forsberg@xhockeypro.com"].sort)
+      expect(parser.emails.sort).to eq ["jonas.forsberg@mynewsdesk.com", "alfred.pennyworth@gotham.travel", "peter.forsberg@xhockeypro.com"].sort
+    end
+
+    it 'supports top-level-domains of all sizes' do
+      parser = Colander::Parser::Plain.new("spec/fixtures/plain.csv")
+      parser.parse
+      expect(parser.emails.sort).to eq ["jonas.forsberg@mynewsdesk.com", "alfred.pennyworth@gotham.travel", "peter.forsberg@xhockeypro.com"].sort
     end
 
     it "retreives emails from an txt file" do
